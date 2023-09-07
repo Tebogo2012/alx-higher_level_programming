cbc4c74d41a2d151850a0c9976a948e6516ae4cc
Tebogo2012 <tebogogti@gmail.com>
2023-09-07 11:03:37 -0700

    task

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,37 @@
-ay_my_name.py
-"""Defines a name-printing function."""
-def say_my_name(first_name, last_name=""):
-        """Print a name.
-            Args:
-                    first_name (str): The first name to print.
-                            last_name (str): The last name to print.
-                                Raises:
-                                        TypeError: If either of first_name or last_name are not strings.
-                                            """
-                                                if not isinstance(first_name, str):
-                                                            raise TypeError("first_name must be a string")
-                                                            if not isinstance(last_name, str):
-                                                                        raise TypeError("last_name must be a string")
-                                                                        print("My name is {} {}".format(first_name, last_name))
+ ago
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+ 10
+ 11
+ 12
+ 13
+ 14
+ 15
+ 16
+ 17
+ 18
+ #!/usr/bin/python3
+ """Defines a name-printing function."""
+
+
+ def say_my_name(first_name, last_name=""):
+         """Print a name.
+
+             Args:
+                     first_name (str): The first name to print.
+                             last_name (str): The last name to print.
+                                 Raises:
+                                         TypeError: If either of first_name or last_name are not strings.
+                                             """
+                                                 if not isinstance(first_name, str):
+                                                             raise TypeError("first_name must be a string")
+                                                             if not isinstance(last_name, str):
+                                                                         raise TypeError("last_name must be a string")
+                                                                         print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,17 +1,43 @@
-quare.py
-"""Defines a square-printing function."""
-def print_square(size):
-        """Print a square with the # character.
-            Args:
-                    size (int): The height/width of the square.
-                        Raises:
-                                TypeError: If size is not an integer.
-                                        ValueError: If size is < 0
-                                            """
-                                                if not isinstance(size, int):
-                                                            raise TypeError("size must be an integer")
-                                                            if size < 0:
-                                                                        raise ValueError("size must be >= 0")
-                                                                        for i in range(size):
-                                                                                    [print("#", end="") for j in range(size)]
-                                                                                            print("")
+ ago
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+ 10
+ 11
+ 12
+ 13
+ 14
+ 15
+ 16
+ 17
+ 18
+ 19
+ 20
+ 21
+ #!/usr/bin/python3
+ """Defines a square-printing function."""
+
+
+ def print_square(size):
+         """Print a square with the # character.
+
+             Args:
+                     size (int): The height/width of the square.
+                         Raises:
+                                 TypeError: If size is not an integer.
+                                         ValueError: If size is < 0
+                                             """
+                                                 if not isinstance(size, int):
+                                                             raise TypeError("size must be an integer")
+                                                             if size < 0:
+                                                                         raise ValueError("size must be >= 0")
+
+                                                                         for i in range(size):
+                                                                                     [print("#", end="") for j in range(size)]
+                                                                                             print("")
